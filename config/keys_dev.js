5b4f0e5d57b54184bfc9811788718d0726dd559d
Kirill Dice <benford.mark783@gmail.com>
2018-06-22 12:30:39 +0300

    Add files via upload

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://sevendice:kuZ8KV@ds233895.mlab.com:33895/devconnector',
+  googleClientID:'1043636370679-mpn52c7b4ekur99e3mp87sosgpos2fmo.apps.googleusercontent.com',
+  googleClientSecret:'4Tv8mfKxtmHqqaQx5hCjXRkS'
+}
